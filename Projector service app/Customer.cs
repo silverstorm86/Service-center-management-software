d5b09eb3cead11bfe04e20f40a5e2709737cd5bb
silverstorm86 <73432898+silverstorm86@users.noreply.github.com>
2020-11-01 20:16:39 +0100

    Creating 2020.11.01
    Creating the program basic structures

---
+++ Projector service app/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projector_service_app
+{
+    //Class for the customers data
+    class Customer
+    {
+    }
+}
