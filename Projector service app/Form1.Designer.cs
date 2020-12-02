a6f8441ddfbc04aff8304b8afd4133a6b7d3021c
silverstorm86 <73432898+silverstorm86@users.noreply.github.com>
2020-12-02 20:31:14 +0100

    Adding menu buttons and some window style
    Adding the menu buttons and some labels. These will be the part of the navigation. Disabling the ControlBox and changing FormBorderStyle to none. Adding rounded border with GraphicsPath and BorderRounded method.

--- Projector service app/Form1.Designer.cs
+++ Projector service app/Form1.Designer.cs
@@ -28,21 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.CustomerLabel = new System.Windows.Forms.Label();
+            this.DevicesLabel = new System.Windows.Forms.Label();
+            this.ExitLabel = new System.Windows.Forms.Label();
+            this.Status = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Devices = new System.Windows.Forms.PictureBox();
+            this.Exit = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Status)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Devices)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Exit)).BeginInit();
             this.SuspendLayout();
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.Location = new System.Drawing.Point(10, 66);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(55, 22);
+            this.StatusLabel.TabIndex = 0;
+            this.StatusLabel.Text = "Status";
+            // 
+            // CustomerLabel
+            // 
+            this.CustomerLabel.AutoSize = true;
+            this.CustomerLabel.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerLabel.Location = new System.Drawing.Point(80, 66);
+            this.CustomerLabel.Name = "CustomerLabel";
+            this.CustomerLabel.Size = new System.Drawing.Size(89, 22);
+            this.CustomerLabel.TabIndex = 1;
+            this.CustomerLabel.Text = "Customers";
+            // 
+            // DevicesLabel
+            // 
+            this.DevicesLabel.AutoSize = true;
+            this.DevicesLabel.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DevicesLabel.Location = new System.Drawing.Point(181, 65);
+            this.DevicesLabel.Name = "DevicesLabel";
+            this.DevicesLabel.Size = new System.Drawing.Size(68, 22);
+            this.DevicesLabel.TabIndex = 2;
+            this.DevicesLabel.Text = "Devices";
+            // 
+            // ExitLabel
+            // 
+            this.ExitLabel.AutoSize = true;
+            this.ExitLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitLabel.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitLabel.Location = new System.Drawing.Point(276, 65);
+            this.ExitLabel.Name = "ExitLabel";
+            this.ExitLabel.Size = new System.Drawing.Size(38, 22);
+            this.ExitLabel.TabIndex = 3;
+            this.ExitLabel.Text = "Exit";
+            this.ExitLabel.Click += new System.EventHandler(this.ExitLabel_Click);
+            // 
+            // Status
+            // 
+            this.Status.Image = global::Projector_service_app.Properties.Resources.volume;
+            this.Status.Location = new System.Drawing.Point(12, 12);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(50, 50);
+            this.Status.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Status.TabIndex = 7;
+            this.Status.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Projector_service_app.Properties.Resources.profle;
+            this.pictureBox1.Location = new System.Drawing.Point(102, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Devices
+            // 
+            this.Devices.Image = global::Projector_service_app.Properties.Resources.videocameracompact;
+            this.Devices.Location = new System.Drawing.Point(191, 12);
+            this.Devices.Name = "Devices";
+            this.Devices.Size = new System.Drawing.Size(50, 50);
+            this.Devices.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Devices.TabIndex = 5;
+            this.Devices.TabStop = false;
+            // 
+            // Exit
+            // 
+            this.Exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Exit.Image = global::Projector_service_app.Properties.Resources.power;
+            this.Exit.Location = new System.Drawing.Point(270, 12);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(50, 50);
+            this.Exit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Exit.TabIndex = 4;
+            this.Exit.TabStop = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(333, 94);
+            this.ControlBox = false;
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Devices);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.ExitLabel);
+            this.Controls.Add(this.DevicesLabel);
+            this.Controls.Add(this.CustomerLabel);
+            this.Controls.Add(this.StatusLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.Status)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Devices)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Exit)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label CustomerLabel;
+        private System.Windows.Forms.Label DevicesLabel;
+        private System.Windows.Forms.Label ExitLabel;
+        private System.Windows.Forms.PictureBox Exit;
+        private System.Windows.Forms.PictureBox Devices;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Status;
     }
 }
 
--- Projector service app/Form1.cs
+++ Projector service app/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +19,32 @@ namespace Projector_service_app
         public Form1()
         {
             InitializeComponent();
+            BorderRounded();
         }
+        private void BorderRounded()
+        {
+            GraphicsPath graphicpath = new GraphicsPath();
+            graphicpath.StartFigure();
+            graphicpath.AddArc(0, 0, 40, 40, 180, 90);
+            graphicpath.AddLine(40, 0, this.Width - 40, 0);
+            graphicpath.AddArc(this.Width - 40, 0, 40, 40, 270, 90);
+            graphicpath.AddLine(this.Width, 40, this.Width, this.Height - 25);
+            graphicpath.AddArc(this.Width - 40, this.Height - 40, 40, 40, 0, 90);
+            graphicpath.AddLine(this.Width - 40, this.Height, 40, this.Height);
+            graphicpath.AddArc(0, this.Height - 40, 40, 40, 90, 90);
+            graphicpath.CloseFigure();
+            this.Region = new Region(graphicpath);
+        }
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ExitLabel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
 
         /*private void button1_Click(object sender, EventArgs e)
         {
